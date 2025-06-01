5358619a1f67b109f618c809216515b907398666
MartinVR009 <martinvasquezr@outlook.com>
2025-05-31 22:01:51 -0500

    usuario admin creado

--- backend/db.js
+++ backend/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'root',
-  password: '',
+  user: 'adminweb',
+  password: 'admin123',
   database: 'estudiantes'
 });
 
